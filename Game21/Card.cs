fee1b357a6061bddb4c784adc3023f62902d5e80
student <student@A28-23>
2022-10-23 09:58:45 +0300

    Add project files.

---
+++ Game21/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game21
+{
+    internal class Card
+    {
+        public enum Shape
+        {
+            SPADE,
+            DIAMOND,
+            HEART,
+            CLUB
+        }
+        private int value;
+        private Shape shape;
+        public Card(Shape shape, int value)
+        {
+            this.shape = shape;
+            this.value = value;
+        }
+        public Shape GetShape() { return shape; }
+        public int GetValue() { return value; }
+
+        public string GetDesc() { return "...."; }//(Description)
+    }
+}
